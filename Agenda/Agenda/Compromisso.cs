34943d979cc65c134eaa77e2996a3b13015bb7fd
richardsdx <richardsdx@hotmail.com>
2019-09-02 19:40:33 -0300

    Primeiro Commit

---
+++ Agenda/Agenda/Compromisso.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    [Table=("Compromisso")]
+    public class Compromisso
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [MinLenght(5)]
+        [MaxLenght(200)]
+        public String Titulo { get; set; }
+
+        [MaxLenght(500)]
+        public String Local { get; set; }
+
+        [Required]
+        public DateTime DataInicio { get; set; }
+
+        public DateTime DataFim { get; set;  }
+
+        public IList<Contato> Participantes { get; set; }
+
+    }
+}
---
+++ Agenda/Agenda/Contato.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    public class Contato
+    {
+        [Table("Contato")]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLenght(3)]
+        [MaxLenght(100)]
+
+        public String Nome { get; set; }
+
+        [Required]
+        [MinLenght(2)]
+        [MaxLenght(100)]
+
+        public String SobreNome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        [MaxLenght(20)]
+
+        public String Telefone { get; set; }
+        
+        public IList<Compromisso> Compromissos { get; set; }
+    }
+}
